3c2293ea6497b882120117032a62fddc00dfa3fa
eparizoto <eparizoto@gmail.com>
2023-04-18 08:36:53 -0300

    commit inicial

---
+++ src/main/java/com/example/testrevert/TestrevertApplication.java
@@ -0,0 +1,15 @@
+package com.example.testrevert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrevertApplication {
+
+	public static void main(String[] args) {
+		System.out.println("teste 1");
+
+		SpringApplication.run(TestrevertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testrevert/TestrevertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrevert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrevertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
